d715579ea9ba1b8cdc7d00baff794c76d360041b
KsinghP <49181830+KsinghP@users.noreply.github.com>
2021-03-13 13:16:33 -0600

    Reads in training tweets
    Acts as the base module for for Deep_Learning_Model and Choose_Save_Best_Model

---
+++ Import_process_split_tweets.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  7 00:06:04 2021
+
+@author: Prabhat
+"""
+import pandas as pd
+import numpy as np
+import re
+import string
+
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords', quiet=True)
+stop = stopwords.words('english')
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+
+from sklearn.model_selection import train_test_split
+
+def import_tweets():
+    path = r'C:\Users\Prabhat\Documents\GitHub\Twitter-Project\Model'
+    filename = r'\latest_training_data.csv'
+    tweets_text_df = pd.read_csv(path + filename)
+    
+    return tweets_text_df
+
+def process_tweets():
+    tweets_text_df = import_tweets()
+    
+    # assign 1 to non-conspiratorial tweets and 0 to conspiratorial ones  
+    #tweets_text_df['numeric_label'] = np.where(tweets_text_df['label']=='Non-conspiratorial', '1', '0')
+    #tweets_text_df['numeric_label'] = tweets_text_df['numeric_label']
+    
+    # lowercase, remove symbols and punctuation
+    tweets_text_df['text'] = tweets_text_df['text'].apply(lambda x: str.lower(x))
+    tweets_text_df['text'] = tweets_text_df['text'].apply(lambda x: re.sub("(&amp?)", "", x))
+    tweets_text_df['text'] = tweets_text_df['text'].apply(lambda x: re.sub("(@.*?)\s", "", x))
+    tweets_text_df["text"] = tweets_text_df["text"].apply(lambda x: re.sub('[%s]' % re.escape(string.punctuation),'',x))
+    
+    # remove "not" from stop words (this increases accuracy of model)
+    stop_words = set(stopwords.words('english')) - set(['not'])
+    tweets_text_df["text"] = tweets_text_df['text'].apply(lambda x: ' '.join([word for word in x.split() if word not in stop_words]))
+    
+    #lemmatize cleaned tweets
+    tweets_text_df["text"] = tweets_text_df["text"].apply(lambda x: ''.join(lemmatizer.lemmatize(x)))
+    
+    return tweets_text_df
+
+def split_tweets():
+    tweets_text_df = process_tweets() 
+    x_train, x_test, y_train, y_test = train_test_split(tweets_text_df["text"], tweets_text_df["label"], test_size=0.25, random_state=1000)
+
+    return (x_train, x_test, y_train, y_test)
