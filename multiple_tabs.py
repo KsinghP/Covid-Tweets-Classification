0f61ce2c0e5118b28f9211819b3d875a20963b16
KsinghP <49181830+KsinghP@users.noreply.github.com>
2021-03-13 00:48:11 -0600

    Update multiple_tabs.py

--- multiple_tabs.py
+++ multiple_tabs.py
@@ -178,30 +178,32 @@ def tweets_user_extract(screen_name):
     #make initial request for most recent tweets (200 is the maximum allowed count)
     try:
         new_tweets = api.user_timeline(screen_name = screen_name, count=200)
-    
-        #save most recent tweets
-        alltweets.extend(new_tweets)
-    
-        #save the id of the oldest tweet less one
-        oldest = alltweets[-1].id - 1
-    
-        #keep grabbing tweets until there are no tweets left to grab
-        while len(new_tweets) > 0:
-            print(f"getting tweets before {oldest}")
-        
-            #all subsiquent requests use the max_id param to prevent duplicates
-            new_tweets = api.user_timeline(screen_name = screen_name, count=200, max_id=oldest)
-        
+        if not new_tweets:
+            st.write("This handle does not exist")
+        else:
             #save most recent tweets
             alltweets.extend(new_tweets)
         
-            #update the id of the oldest tweet less one
+            #save the id of the oldest tweet less one
             oldest = alltweets[-1].id - 1
-    
-        #transform the tweepy tweets into a 2D array that will populate the csv 
-        user_tweets = [tweet.text for tweet in alltweets]
-    
-        filter_user_tweets(user_tweets)
+        
+            #keep grabbing tweets until there are no tweets left to grab
+            while len(new_tweets) > 0:
+                print(f"getting tweets before {oldest}")
+            
+                #all subsiquent requests use the max_id param to prevent duplicates
+                new_tweets = api.user_timeline(screen_name = screen_name, count=200, max_id=oldest)
+            
+                #save most recent tweets
+                alltweets.extend(new_tweets)
+            
+                #update the id of the oldest tweet less one
+                oldest = alltweets[-1].id - 1
+        
+            #transform the tweepy tweets into a 2D array that will populate the csv 
+            user_tweets = [tweet.text for tweet in alltweets]
+        
+            filter_user_tweets(user_tweets)
     
     except:
         st.write("")
