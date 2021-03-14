637d777c23f7d47cd9c3404f8644daaf0f1f2552
KsinghP <49181830+KsinghP@users.noreply.github.com>
2021-03-13 21:35:25 -0600

    Update multiple_tabs.py

--- multiple_tabs.py
+++ multiple_tabs.py
@@ -304,15 +304,17 @@ def about_page():
     st.title("Navigating Covid Misinformation on Twitter")
     st.write("Hello, welcome to my app, an attempt to understand and combat Covid-related misinformation on Twitter. Given the sea of covid misinformation out there, there's a good chance you've encountered tweets that are either borderline or outright misinformation. No one can fault you for peeking at handles making these tweets to check whether they're first-time offenders or serious vectors of misinformation. But of course it's near-impossible to go through a handle's tweet history, which is where this app comes in. You can simply input the handle of concern and know whether or not it regularly tweets covid misinformation. In addition, you can also enter certain keywords and check to what extent they're associated with conspiratorial covid tweets.")		
     st.write("**This app works by running [my project](https://github.com/KsinghP/Covid-Tweets-Classification) in the background.**")
-    st.info('To understand how to use the app, navigate to the "How to Use" section using the menu on the left hand side.')
+    st.info('To understand how to use the app, navigate to the **How to Use** section ')
 
     
 def instructions_for_use():
     st.write("Users can provide two inputs:")
     st.text("")
-    st.write("1.in the **Run Keyword Feature**, they can enter one or more keywords from the list below and also enter the number of tweets whose conspiratorial vs. non-conspiratorial breakup they want to see")
+    st.write("1.in the **Run Keyword Feature**, they can enter one or more keywords from the list below and also enter the number of tweets whose conspiratorial vs. non-conspiratorial breakup they want to see.")
     st.write("2.in the **Run Twitter Handle Feature**, they can enter a twitter handle.")
-    #st.markdown("To extract tweets based on one or more of multiple keywords, please separate them by OR.")
+    st.markdown ("List of keywords to choose from ")
+    st.info("[sars-cov-2, sarscov2 corona, mask, vaccine, pfizer, moderna, astra zeneca, astrazeneca, social distancing, socialdistancing, coronavirus, covid, covid19, covid-19, wuhan, china virus, china plague, chinavirus, hcq, hydroxychloroquine, shutdown, herdimmunity, herd immunity, vaccine, scamdemic, plandemic, fauci, bill gates, kung flu, kungflu, quarantine, lockdown]")
+    st.markdown("To extract tweets based on one or more of multiple keywords, please separate them by OR.")
     #st.markdown("To extract tweets based on multiple keywords, please separate them only by a space.")
     #st.info("For example, to extract tweets based on at least one of the keywords mask, social distancing and lockdown, enter: mask OR social distancing OR lockdown")
     #st.info("Similarly, to extract tweets based on all of the keywords mask, social distancing and lockdown, enter: mask social distancing lockdown")
